59b8ac911214a6f30157ce3d6d72686604433396
Your Name <you@example.com>
2026-02-19 11:37:08 -0400

    LOCALIZACAO DO MOTORISTA

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import App from "./App";
+
+import marker2x from "leaflet/dist/images/marker-icon-2x.png";
+import marker1x from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: marker2x,
+  iconUrl: marker1x,
+  shadowUrl: markerShadow,
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
